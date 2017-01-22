e4147553ae2b05726d11815002a8617206cf768d
Hangil Chang <hangil@chang.jp>
2017-01-23 08:21:35 +0900

    added svg preview

--- generators/app/templates/browser-src/preview-main.ts
+++ generators/app/templates/browser-src/preview-main.ts
@@ -1,4 +1,4 @@
-import { Compiler } from "myclinic-drawer";
+import { Compiler, drawerToSvg } from "myclinic-drawer";
 import * as service from "./service";
 
 let data = window["data"];
@@ -7,4 +7,13 @@ let comp = new Compiler();
 comp.moveTo(data.x, data.y);
 comp.lineTo(100, 200);
 let ops = comp.getOps();
-service.print([ops]);
+let previewArea = document.getElementById("preview-wrapper");
+let previewSvg = drawerToSvg(ops, {
+	width: "210mm",
+	height: "297mm",
+	viewBox: "0 0 210 297"
+});
+if( previewArea !== null ){
+	previewArea.appendChild(previewSvg);
+}
+//service.print([ops]);
